cb0e89bb10bbeec5286531e99b924e4615fe44cd
Fero Klucar (PWC) <frantisek.klucar@pwc.com>
2023-07-11 12:38:09 +0100

    ♻️ refactor code

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,32 +1,38 @@
-
-const URL = "https://itx-frontend-test.onrender.com/api";
-const requestOptions = {
-    redirect: 'follow'
-};
-
+const URL = 'https://itx-frontend-test.onrender.com/api';
 
 export class ProductService {
-    async readProducts() {
-        requestOptions.method = 'GET';
-        return fetch(URL + "/product", requestOptions)
-            .then(response => response.json())
-    }
+	async readProducts() {
+		const requestOptions = {
+			redirect: 'follow',
+			method: 'GET',
+		};
+		return fetch(URL + '/product', requestOptions).then((response) => response.json());
+	}
 
-    async readProductById(productId) {
-        requestOptions.method = 'GET';
-        return fetch(URL + "/product/" + productId, requestOptions)
-            .then(response => response.json())
-    }
+	async readProductById(productId) {
+		const requestOptions = {
+			redirect: 'follow',
+			method: 'GET',
+		};
+		return fetch(URL + '/product/' + productId, requestOptions).then((response) =>
+			response.json()
+		);
+	}
 
-    async addProductToCart(productId, colorCode, storageCode) {
-        const raw = { "id": productId, "colorCode": colorCode, "storageCode": storageCode }
+	async addProductToCart(productId, colorCode, storageCode) {
+		const body = JSON.stringify({
+			id: productId,
+			colorCode: colorCode,
+			storageCode: storageCode,
+		});
+		
+		const requestOptions = {
+			redirect: 'follow',
+			method: 'POST',
+			body: body,
+			headers: { 'Content-Type': 'application/json' },
+		};
 
-        requestOptions.method = 'POST';
-        requestOptions.body = raw;
-        requestOptions.mode = "no-cors";
-        requestOptions.headers = { 'Content-Type': 'application/json' };
-
-        return fetch(URL + "/cart", requestOptions)
-            .then(response => response)
-    }
+		return fetch(URL + '/cart', requestOptions).then((response) => response.json());
+	}
 }
