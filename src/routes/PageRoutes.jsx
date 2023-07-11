97943a0bad0528d8923593e089661ee6481781de
Fero Klucar (PWC) <frantisek.klucar@pwc.com>
2023-07-11 17:12:00 +0100

    🎨 Format code

--- src/routes/PageRoutes.jsx
+++ src/routes/PageRoutes.jsx
@@ -2,12 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import { ProductListPage, ProductDetailsPage } from '../pages';
 
 function PageRoutes() {
-    return (
-        <Routes>
-            <Route path='/' element={<ProductListPage />} />
-            <Route path='/product/:id' element={<ProductDetailsPage />} />
-        </Routes>
-    );
+	return (
+		<Routes>
+			<Route path="/" element={<ProductListPage />} />
+			<Route path="/product/:id" element={<ProductDetailsPage />} />
+		</Routes>
+	);
 }
 
 export default PageRoutes;
