4b66b31bf04aebaf1eeea4cd866a94c01dea3e62
Wolfero <ferorock44@gmail.com>
2023-07-11 01:22:08 +0100

    ♻ Refactor components imports

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { Item } from "./Item";
+import { Description } from "./Description"
+import { Actions } from "./Actions"
+import { Spinner } from "./Spinner"
+
+export { Header, Footer, Item, Description, Actions, Spinner };
