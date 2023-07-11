192fce778e6c777ea667bed3b766c9c97c8351da
Fero Klucar (PWC) <frantisek.klucar@pwc.com>
2023-07-11 12:38:49 +0100

    ✨ Add storage of data

---
+++ src/services/StorageService.js
@@ -0,0 +1,27 @@
+export class StorageData {
+	stashData(data) {
+		localStorage.removeItem('data');
+		var cacheData = {
+			data: data,
+			expiration: new Date().getTime() + 60 * 60 * 1000,
+		};
+		localStorage.setItem('data', JSON.stringify(cacheData));
+	}
+
+	unStashData() {
+		var stashedData = localStorage.getItem('data');
+
+		if (stashedData) {
+			var cacheData = JSON.parse(stashedData);
+
+			if (new Date().getTime() < cacheData.expiration) {
+				return cacheData.data;
+			} else {
+				localStorage.removeItem('data');
+			}
+		}
+
+		// No hay datos almacenados o los datos han expirado
+		return null;
+	}
+}
