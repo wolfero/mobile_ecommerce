706f3468008cc890042467de01f1266fdb7fb779
Fero Klucar (PWC) <frantisek.klucar@pwc.com>
2023-07-11 07:54:59 +0100

    🚚 Update assets folder

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,11 +1,11 @@
 import styles from "./Spinner.module.scss";
 
 const Spinner = () => {
-    return (
-        <div className={styles.SpinnerBox}>
-            <img src="/loader.svg" alt="Loader" />
-        </div>
-    );
+  return (
+    <div className={styles.SpinnerBox}>
+      <img src="./assets/spinner.svg" alt="Loader" />
+    </div>
+  );
 };
 
 export default Spinner;
