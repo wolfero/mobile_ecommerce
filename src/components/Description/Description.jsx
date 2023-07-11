dbfb9fe67c68eabe48ee273f712d3578dbbbc615
Wolfero <ferorock44@gmail.com>
2023-07-12 00:03:27 +0100

    🥅 Add control price and camera errors

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,31 +1,67 @@
 import React from 'react';
 
 function Description({ product }) {
-    return (
-        <div>
-            <h2>{product.brand}</h2>
-            <h3>{product.model}</h3>
-            <p>Precio: {product.price}€</p>
-            <p>CPU: {product.cpu}</p>
-            <p>RAM: {product.ram}</p>
-            <p>Sistema Operativo: {product.os}</p>
-            <p>Resolución de pantalla: {product.displayResolution}</p>
-            <p>Batería: {product.battery}</p>
-            <div>
-                <h4>Cámaras:</h4>
-                <p>
-                    Cámara principal: {product.primaryCamera[0]}{' '}
-                    {product.primaryCamera[1]}
-                </p>
-                <p>
-                    Cámara secundaria: {product.secondaryCmera[0]}{' '}
-                    {product.secondaryCmera[1]}
-                </p>
-            </div>
-            <p>Dimensiones: {product.dimentions}</p>
-            <p>Peso: {product.weight}g</p>
-        </div>
-    );
+	const price = product.price ? product.price + '€' : 'Out of stock';
+	const isString = (productCamera) => typeof productCamera === 'string';
+	const camera = (productCamera) => productCamera.map((camera) => camera + ' ');
+
+	return (
+		<div>
+			<h2>{product.brand}</h2>
+			<h3>{product.model}</h3>
+			<p>
+				<b>Precio: </b>
+				{price}
+			</p>
+			<p>
+				<b>CPU: </b>
+				{product.cpu}
+			</p>
+			<p>
+				<b>RAM: </b>
+				{product.ram}
+			</p>
+			<p>
+				<b>Sistema Operativo: </b>
+				{product.os}
+			</p>
+			<p>
+				<b>Resolución de pantalla: </b>
+				{product.displayResolution}
+			</p>
+			<p>
+				<b>Batería: </b>
+				{product.battery}
+			</p>
+			<div>
+				<h4>Cámaras:</h4>
+				<p>
+					<b>Cámara principal: </b>
+					{isString(product.primaryCamera) ? (
+						<span>{product.primaryCamera}</span>
+					) : (
+						<span>{camera(product.primaryCamera)}</span>
+					)}
+				</p>
+				<p>
+					<b>Cámara secundaria: </b>
+					{isString(product.secondaryCmera) ? (
+						<span>{product.secondaryCmera}</span>
+					) : (
+						<span>{camera(product.secondaryCmera)}</span>
+					)}
+				</p>
+			</div>
+			<p>
+				<b>Dimensiones: </b>
+				{product.dimentions}
+			</p>
+			<p>
+				<b>Peso: </b>
+				{product.weight}g
+			</p>
+		</div>
+	);
 }
 
 export default Description;
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 function Item({ product }) {
-  return (
-    <div className="item">
-      <img src={product.imgUrl} alt={product.model} />
-      <div>
-        <h3>{product.brand}</h3>
-        <p>{product.model}</p>
-        <p>Precio: {product.price}€</p>
-      </div>
-    </div>
-  );
+	const price = product.price ? product.price + '€' : 'Out of stock';
+
+	return (
+		<div className="item">
+			<img src={product.imgUrl} alt={product.model} />
+			<div>
+				<h3>{product.brand}</h3>
+				<p>{product.model}</p>
+				<p>Price: {price}</p>
+			</div>
+		</div>
+	);
 }
 
 export default Item;
