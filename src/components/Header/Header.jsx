f159df2fcede201b58ef8c312f77abe72debda89
Fero Klucar (PWC) <frantisek.klucar@pwc.com>
2023-07-11 17:10:21 +0100

    ⚡️ Add Breadcrumb

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useSection from '../../hooks/useSection';
+import styles from './Header.module.scss';
 
 function Header() {
 	const { data } = useSection();
+	const { pathname } = useLocation();
+	const pathnames = pathname.split('/').filter((item) => item);
 
 	return (
-		<header>
-			<Link to="/">Logo</Link>
-			<nav>
-				//TODO barras navegación
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-				</ul>
+		<header className={styles.Container}>
+			<h1>
+				<Link to="/">Mobile ECommerce</Link>
+			</h1>
+			<nav className={styles.Breadcrumb}>
+				{pathnames.length > 0 ? (
+					<div>
+						<Link to="/" className={styles.Active}>
+							Products
+						</Link>
+						<span className={styles.Arrow}>/</span>
+						<Link className={styles.NotActive}> Details</Link>
+					</div>
+				) : (
+					<Link to="/" className={styles.NotActive}>
+						Products
+					</Link>
+				)}
+				<p className={styles.ProductsCount}>
+					Products in Cart
+					<span>{data.productsInCart}</span>
+				</p>
 			</nav>
-			<div>
-                <span>{data.productsInCart}</span>
-            </div>
 		</header>
 	);
 }
