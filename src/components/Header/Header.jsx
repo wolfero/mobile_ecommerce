42d369501e404ebfd3010b5633e8d57e8aa12baa
Fero Klucar (PWC) <frantisek.klucar@pwc.com>
2023-07-12 13:50:08 +0100

    💄 Update style Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import useSection from '../../hooks/useSection';
-import styles from './Header.module.scss';
 
 function Header() {
 	const { data } = useSection();
@@ -9,28 +8,61 @@ function Header() {
 	const pathnames = pathname.split('/').filter((item) => item);
 
 	return (
-		<header className={styles.Container}>
-			<h1>
-				<Link to="/">Mobile ECommerce</Link>
+		<header className="mx-auto bg-stone-900 p-4">
+			<h1 className="flex justify-center font-bold text-2xl md:text-5xl md:mb-0">
+				<Link to="/" className="uppercase items-center text-stone-300">
+					Mobile ECommerce
+				</Link>
 			</h1>
-			<nav className={styles.Breadcrumb}>
-				{pathnames.length > 0 ? (
-					<div>
-						<Link to="/" className={styles.Active}>
-							Products
-						</Link>
-						<span className={styles.Arrow}>/</span>
-						<Link className={styles.NotActive}> Details</Link>
-					</div>
-				) : (
-					<Link to="/" className={styles.NotActive}>
-						Products
-					</Link>
-				)}
-				<p className={styles.ProductsCount}>
-					<span>{data.productsInCart}</span>
+			<div className="mx-8 flex flex-wrap flex-col md:flex-row md:justify-between items-center">
+				<nav className="flex p-3">
+					<ol className="inline-flex items-center space-x-1 md:space-x-3">
+						{pathnames.length > 0 ? (
+							<>
+								<li className="inline-flex items-center">
+									<Link
+										to="/"
+										className="text-xl text-stone-400 hover:text-stone-200"
+									>
+										Products
+									</Link>
+								</li>
+								<li className="inline-flex items-center">
+									<svg
+										className="w-3 h-3 text-stone-400"
+										aria-hidden="true"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 6 10"
+									>
+										<path
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="m1 9 4-4-4-4"
+										/>
+									</svg>
+								</li>
+								<li className="inline-flex items-center">
+									<span className="text-xl font-bold text-stone-400">
+										Details
+									</span>
+								</li>
+							</>
+						) : (
+							<li className="inline-flex items-center">
+								<span className="text-xl font-bold text-stone-400">Products</span>
+							</li>
+						)}
+					</ol>
+				</nav>
+				<p className="px-5">
+					<span className="text-xl font-medium text-stone-300">
+						{data.productsInCart}
+					</span>
 				</p>
-			</nav>
+			</div>
 		</header>
 	);
 }
