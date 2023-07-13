fb7adf470c40d69e251e057ef648b91eb9fb5206
Wolfero <ferorock44@gmail.com>
2023-07-13 01:00:58 +0100

    💄 Update product list styles

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 
 function Item({ product }) {
 	const price = product.price ? (
-		<p className="text-xl font-bold text-stone-600">
+		<p className="pb-2 text-xl font-bold text-stone-600">
 			<span className="text-4xl">{product.price}</span>€
 		</p>
 	) : (
-		<p className="mt-1 bg-red-500 text-3xl font-bold uppercase text-white">Out of stock</p>
+		<p className="bg-red-500 py-2 text-2xl font-bold uppercase text-white">Out of stock</p>
 	);
 
 	return (
 		<div className="flex flex-col overflow-hidden rounded-lg bg-white shadow-xl">
 			<img src={product.imgUrl} alt={product.model} className="mx-auto my-2 flex h-60" />
-			<p className="uppercase text-stone-600">
-				{product.brand} {product.model}
-			</p>
+			<p className="text-lg uppercase text-stone-600">{product.brand}</p>
+			<p className="text-lg uppercase text-stone-600">{product.model}</p>
 			{price}
 		</div>
 	);
--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -30,7 +30,7 @@ function ProductListPage() {
 	}, [data.products, searchText]);
 
 	return (
-		<div className="m-8 flex flex-col rounded-2xl bg-stone-200 p-4 align-middle">
+		<div className="m-8 flex flex-col rounded-2xl bg-stone-200 p-4 text-center align-middle text-stone-600">
 			<section className="container mx-auto flex flex-col px-1">
 				<input
 					className="w-full self-center rounded-lg px-8 py-4 md:w-1/2 md:self-end"
@@ -40,11 +40,11 @@ function ProductListPage() {
 					placeholder="Search products"
 				/>
 				{productsData ? (
-					<section className="flex flex-wrap justify-center pt-2">
+					<section className="flex flex-wrap justify-center gap-2 pt-2 md:gap-0">
 						{productsData.map((product) => (
 							<article
 								key={product.id}
-								className="w-full max-w-sm transform cursor-pointer px-4 py-6 duration-300 hover:-translate-y-2 sm:w-1/2 lg:w-1/4"
+								className="w-full max-w-sm transform cursor-pointer duration-300 hover:-translate-y-2 md:w-1/2 lg:w-1/4 md:p-2"
 							>
 								<Link to={`/product/${product.id}`}>
 									<Item product={product} />
