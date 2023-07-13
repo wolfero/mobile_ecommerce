455c3fd0c0a5145e9e7f27bb9073deb476d78a39
Fero Klucar (PWC) <frantisek.klucar@pwc.com>
2023-07-13 13:36:38 +0100

    💄 Update Product description style

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 function Description({ product }) {
 	const drawPrice = (price) => {
 		const data = price ? (
-			<p className="pb-2 text-xl font-bold text-stone-600">
-				<span className="text-4xl">{product.price}</span>€
-			</p>
+			<span className="">{product.price}€</span>
 		) : (
 			<span className="rounded-md bg-red-500 px-2 py-1 font-bold uppercase text-white">
 				Out of stock
@@ -17,55 +15,40 @@ function Description({ product }) {
 	const drawCamera = (camera) => {
 		const isString = typeof camera === 'string';
 		const getCameraData = (cameraData) => cameraData.map((data) => data + ' ');
-		return <p>{isString ? camera : getCameraData(camera)}</p>;
+		return isString ? camera : getCameraData(camera);
+	};
+
+	const detailsBuilder = (title, detail) => {
+		const drawDetail = detail ? detail : '-';
+		return (
+			<article className="flex flex-col justify-between gap-2 border-t border-stone-300 p-2 text-stone-600 md:flex-row">
+				<b>{title}</b>
+				<p>{drawDetail}</p>
+			</article>
+		);
 	};
 
 	return (
 		<div className=" rounded-lg bg-white p-4 shadow-xl">
 			<header>
-				<h2 class="title-font text-2xl tracking-widest text-stone-500">{product.brand}</h2>
-				<h1 class="title-font mb-1 text-3xl font-medium text-stone-900">{product.model}</h1>
+				<h2 className="title-font text-2xl tracking-widest text-stone-500">
+					{product.brand}
+				</h2>
+				<h1 className="title-font mb-1 text-3xl font-medium text-stone-900">
+					{product.model}
+				</h1>
 			</header>
-			<article>
-				<b>Precio: </b>
-				{drawPrice(product.price)}
-			</article>
-			<article>
-				<b>CPU: </b>
-				{product.cpu}
-			</article>
-			<article>
-				<b>RAM: </b>
-				{product.ram}
-			</article>
-			<article>
-				<b>Sistema Operativo: </b>
-				{product.os}
-			</article>
-			<article>
-				<b>Resolución de pantalla: </b>
-				{product.displayResolution}
-			</article>
-			<article>
-				<b>Batería: </b>
-				{product.battery}
-			</article>
-			<article>
-				<b>Cámara principal: </b>
-				<span>{drawCamera(product.primaryCamera)}</span>
-			</article>
-			<article>
-				<b>Cámara secundaria: </b>
-				<span>{drawCamera(product.secondaryCmera)}</span>
-			</article>
-			<article>
-				<b>Dimensiones: </b>
-				{product.dimentions}
-			</article>
-			<article>
-				<b>Peso: </b>
-				{product.weight}g
-			</article>
+
+			{detailsBuilder('OS', product.os)}
+			{detailsBuilder('CPU', product.cpu)}
+			{detailsBuilder('RAM', product.ram)}
+			{detailsBuilder('Battery', product.battery)}
+			{detailsBuilder('Dimensions', product.dimentions)}
+			{detailsBuilder('Display resolution', product.displayResolution)}
+			{detailsBuilder('Primary camera', drawCamera(product.primaryCamera))}
+			{detailsBuilder('Secondary camera', drawCamera(product.secondaryCmera))}
+			{detailsBuilder('Wight', product.weight)}
+			{detailsBuilder('Price', drawPrice(product.price))}
 		</div>
 	);
 }
